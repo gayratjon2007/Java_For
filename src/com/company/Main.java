fbc628623368d981295b35d212ca4c49360900b7
IT school <gayratjon2007>
2022-07-27 23:29:56 -0400

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
+        Scanner scanner = new Scanner(in);
 
     }
 }
