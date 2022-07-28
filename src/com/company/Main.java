6d0a24611274aa3d4b4652e835ff4eb5ccf13b31
IT school <gayratjon2007>
2022-07-28 00:33:10 -0400

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,14 @@ public class Main {
 	// write your code here
 
         Scanner scanner = new Scanner(in);
+        System.out.print("ko`paytruvchi raqamni kriting");
+        int q = scanner. nextInt();
+        int a = 1;
+        for (int i = 1; i <=10 ; i++) {
+            a =q * i;
+            System.out.println(q + " * " +i+ " = " +a);
+
+        }
 
     }
 }
